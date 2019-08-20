9acad1c423249b5b0f971ace4d43ac0f0a8a11d2
benjopw <benyaminpurbowasito@gmail.com>
2019-08-20 15:35:58 +0700

    Initial Commit

---
+++ CustomAnnotationPin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomAnnotationPin
+//
+//  Created by Benjamin Purbowasito on 20/08/19.
+//  Copyright © 2019 iosda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
