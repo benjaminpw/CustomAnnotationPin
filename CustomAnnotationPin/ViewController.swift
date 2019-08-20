0317221975662430c4e5f9dc4b1ec839060a9d46
benjopw <benyaminpurbowasito@gmail.com>
2019-08-20 15:49:03 +0700

    annotation pin location

--- CustomAnnotationPin/ViewController.swift
+++ CustomAnnotationPin/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        let location = CLLocationCoordinate2D(latitude: -6.175392, longitude: 106.827153)
+        let region = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+        self.mapView.setRegion(region, animated: true)
     }
 
 
