614404ecb586a5e10bc0b080f8d5d1a0827c7879
benjopw <benyaminpurbowasito@gmail.com>
2019-08-21 17:28:02 +0700

    revisi 2.0

--- CustomAnnotationPin/ViewController.swift
+++ CustomAnnotationPin/ViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 import MapKit
-import CoreLocation
 
 class customPin: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
     var title: String?
     var subtitle: String?
-    
+
     init(pinTitle: String, pinSubTitle: String, location:CLLocationCoordinate2D) {
         self.title = pinTitle
         self.subtitle = pinSubTitle
@@ -22,21 +21,21 @@ class customPin: NSObject, MKAnnotation {
     }
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    
     @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         let location = CLLocationCoordinate2D(latitude: -6.175392, longitude: 106.827153)
         //itu lattitude sama longitude monas, kaya buat default aja
-        let region = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+        let region = MKCoordinateRegion (center: location , span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
         self.mapView.setRegion(region, animated: true)
-        
         let pin = customPin(pinTitle: "monas", pinSubTitle: "jakarta", location: location)
         self.mapView.addAnnotation(pin)
         self.mapView.delegate = self
-    }
+        
 
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
@@ -47,50 +46,10 @@ class ViewController: UIViewController {
         annotationView.canShowCallout = true
         return annotationView
     }
-    
+
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         print("annotation title == \(String(describing:view.annotation?.title!))")
     }
-}
 
-func getCenterLocation(for mapView: MKMapView) -> CLLocation {
-    let latitude = mapView.centerCoordinate.latitude
-    let longitude = mapView.centerCoordinate.longitude
-    
-    return CLLocation (latitude: latitude, longitude: longitude)
 }
-
-extension ViewController: MKMapViewDelegate {
-    
-    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        let center = getCenterLocation (for: mapView)
-        let geoCoder = CLGeocoder()
-        
-        guard let previousLocation = self.previousLocation else { return }
-        
-        guard center.distance(from: previousLocation) > 50 else { return }
-        self.previousLocation = center
-        
-        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
-            guard let self = self else { return }
-            
-            if let _ = error {
-                //TODO: Show alert informing the user
-                return
-            }
-            
-            guard let placemark = placemarks?.first else {
-                //TODO: Show alert informing the user
-                return
-            }
-            
-            let streetNumber = placemark.subThoroughfare ?? ""
-            let streetName = placemark.thoroughfare ?? ""
-            
-            DispatchQueue.main.async {
-                self.addressLabel.text = "\(streetNumber) \(streetName)"
-            }
-        }
-    }
 }
-
